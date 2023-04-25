b764eec365ea126cd0a3efe48ba18f73c9aa41d8
Marta <mgomezdeleon@ceinmark.net>
2023-04-25 15:55:39 +0200

    Modificado por ceinmark

--- src/Hola.java
+++ src/Hola.java
@@ -4,6 +4,6 @@ public class Hola {
 	public static void main(String[] args) {
 		System.out.println("Hola, soy Ceinmark");
 		System.out.println("Hola, soy Marta");
-		
+		System.out.println("Modificado por Ceinmark");
 	}
 }
